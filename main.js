f413bab81a01c3b8183f481be04b8655dd1ffe5d
Luca Veca <lucaveca@gmail.com>
2014-09-06 17:04:38 -0700

    Added new button plus bootstrap
    button for guitar, bootstrap css and javascript

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ $(function() {
       collapsible: true
     });
   });
+
+var txt = $( ".btn-split" ).text();
+$( ".btn-split" ).attr("name", txt);
+
+var txt = $( ".btn-loop" ).text();
+$( ".btn-loop" ).attr("name", txt);
+
