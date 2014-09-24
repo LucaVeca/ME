caf6e062366aacbd1d6481c177c771cacf107c14
Luca Veca <lucaveca@gmail.com>
2014-09-24 11:18:28 -0700

    new responsive sticky nav
    sticky nav with social icons

--- main.js
+++ main.js
@@ -390,3 +390,185 @@ $(document).ready(function() {
 });
 
 
+$(function(){
+  'use strict';
+
+  // Slider functions
+  function sliderPane(){
+
+    function slide(){
+      var sliderCtx = $('#team-slider');
+      var slider = sliderCtx.find('.slider');
+      var sliderWidth = slider.width();
+      var slideWidth = sliderWidth/3;
+
+      var arrLeft = $('.slider-controls .control-left');
+      var arrRight = $('.slider-controls .control-right');
+
+      var bullets = $('.slider-bullets .bullet a');
+
+
+      // Some functions for the slider 
+      function slideTo($url){
+
+        function addCurrent($num){
+          $('.slider').attr('data-slide', $num);
+        }
+        slider.css({
+          marginLeft: 0
+        });
+        if($url === '#1'){
+          slider.css({
+            marginLeft: 0
+          });
+
+          addCurrent('1');
+        }
+        else if($url === '#2'){
+          slider.css({
+            marginLeft: '-'+slideWidth + 'px'
+          });
+
+          addCurrent('2');
+        }
+        else{
+          slider.css({
+            marginLeft: '-'+slideWidth*2 + 'px'
+          });
+          addCurrent('3'); 
+        }
+
+      }
+
+      function colorBullets(){
+        var slider = $('.slider');
+
+        $('.team-slider a').on('click', function(){
+          var num = slider.attr('data-slide');
+
+          if(num === '1'){
+            $('.slider-bullets .bullet').removeClass('active');
+            $('.slider-bullets .bullet:nth-child(1)').addClass('active');
+
+          }
+          else if(num === '2'){
+            $('.slider-bullets .bullet').removeClass('active');
+            $('.slider-bullets .bullet:nth-child(2)').addClass('active');
+          }
+          else {
+            $('.slider-bullets .bullet').removeClass('active');
+            $('.slider-bullets .bullet:nth-child(3)').addClass('active');
+          }
+
+        }); 
+      }
+      // Make it slide ! 
+      arrLeft.on('click', function(){ 
+
+        var $this = $(this);
+
+        var url = $this.attr('href');
+        slideTo(url);
+
+        if($this.attr('href') === '#3'){ 
+          $this.attr('href', '#2'); 
+          arrRight.attr('href', '#1'); 
+        }  
+        else if($this.attr('href') === '#2'){
+          $this.attr('href', '#1'); 
+          arrRight.attr('href', '#3');
+        }
+        else {
+          $this.attr('href', '#3');
+          arrRight.attr('href', '#2');
+        }
+
+      });
+      arrRight.on('click', function(){
+        var $this = $(this);
+        var url = $this.attr('href'); 
+        slideTo(url);
+
+        if($this.attr('href') === '#3'){
+          $this.attr('href', '#1');
+          arrLeft.attr('href', '#2'); 
+        }
+        else if($this.attr('href') === '#1'){
+          $this.attr('href', '#2'); 
+          arrLeft.attr('href', '#3');
+        }
+        else {
+          $this.attr('href', '#3');
+          arrLeft.attr('href', '#1');
+        }
+
+      });
+      bullets.on('click', function(){
+        var url = $(this).attr('href');
+        if (url == "#1"){
+          arrLeft.attr('href', '#3');
+          arrRight.attr('href', '#2');
+        }
+        else if (url == "#2"){
+          arrLeft.attr('href', '#1');
+          arrRight.attr('href', '#3');
+        }
+        else {
+          arrLeft.attr('href', '#2');
+          arrRight.attr('href', '#1');
+        }
+        
+        slideTo(url);
+      });
+      colorBullets();
+    }
+    slide();
+  }
+  sliderPane();
+})
+
+// Cache selectors
+var lastId,
+    topMenu = $("#topNav"),
+    topMenuHeight = topMenu.outerHeight(),
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      o = href === "#" ? 0 : $(href).offset().top-topMenuHeight+15;
+  $('html, body').stop().animate({ 
+      scrollTop: o
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
